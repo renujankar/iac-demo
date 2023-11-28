5969342cf0bedbecdf572ec672460c920c337b04
Renuka Jankar <jankarrenuka9623@gmail.com>
2023-11-28 23:29:44 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,13 @@
 provider "aws" {
-  access_key = "AKIAZUOSMUTFT3VWU3TI"
-  secret_key = "mKVKAARB7jkvse2/VaNQjDsOEo8c1x2Fd4D6EUq3"
-  region = "ap-south-1"
+  region = "ap-south-1"  # Change this to your desired region
 }
-resource "aws_instance" "server"{
- ami = "0fa1ca9559f1892ec"
- instance_type = "t2.micro"
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "example-vpc"
+  }
 }
